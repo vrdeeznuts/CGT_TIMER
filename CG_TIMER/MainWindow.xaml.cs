59bc14417a31ef262cd42f6494bbd1b8737a414a
yadam <yadam@YUKIPC>
2023-09-05 13:19:41 -0400

    Add project files.

---
+++ CG_TIMER/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using static CGT_TIMER.RelayCommand;
+using static CGT_TIMER.SettingsWindow;
+
+namespace CGT_TIMER
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // mouse interaction objects
+
+
+        // Timer objects
+        private readonly DispatcherTimer dt = new();
+        private readonly Stopwatch sw = new();
+        private readonly TimeSpan initialOffset = TimeSpan.FromSeconds(-10);
+        public TimeSpan InitialOffset => initialOffset;
+
+        // keybinding commands
+        public ICommand ToggleStartPauseCommand { get; private set; }
+        public ICommand ResetCommand { get; private set; }
+        public ICommand SetTTBCommand { get; private set; }
+        public ICommand ClearTTBCommand { get; private set; }
+        public ICommand CloseWindowCommand { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ToggleStartPauseCommand = new RelayCommand(ToggleStartPause);
+            ResetCommand = new RelayCommand(ResetTimer);
+            SetTTBCommand = new RelayCommand(SetTTB);
+            ClearTTBCommand = new RelayCommand(ClearTTB);
+            CloseWindowCommand = new RelayCommand(CloseWindow);
+            dt.Interval = TimeSpan.FromMilliseconds(100);
+#pragma warning disable CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
+            dt.Tick += Timer_Tick;
+#pragma warning restore CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // allows dragging window on screen by holding down anywhere on window
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SettingsWindow settingsWindow = new();
+            settingsWindow.ShowDialog();
+        }
+
+        private void ToggleStartPause()
+        {
+            // keybinding <Space>
+            if (sw.IsRunning)
+            {
+                sw.Stop();
+                dt.Stop();
+            }
+            else
+            {
+                sw.Start();
+                dt.Start();
+            }
+        }
+
+        private void ResetTimer()
+        {
+            // keybinding <Delete>
+            sw.Reset();
+            dt.Stop();
+            UpdateCurrentTimeLabel();
+            CurrentTimeText.Content = "-0:10.0";
+        }
+
+        private void SetTTB()
+        {
+            // keybinding <Ctrl>+<Space>
+            UpdateTimeToBeatText();
+        }
+
+        private void ClearTTB()
+        {
+            // keybinding <Ctrl>+<Delete>
+            TimeToBeatText.Content = "0:00.0";
+        }
+
+        private void CloseWindow()
+        {
+            Close();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateCurrentTimeLabel();
+        }
+
+        private void UpdateCurrentTimeLabel()
+        {
+            TimeSpan elapsed = sw.Elapsed + InitialOffset;
+            if (elapsed.TotalMilliseconds < 0 )
+            {
+                CurrentTimeText.Content = $"-{elapsed:m\\:ss\\.f}";
+            }
+            else
+            {
+                CurrentTimeText.Content = $"{elapsed:m\\:ss\\.f}";
+            }
+        }
+
+        private void UpdateTimeToBeatText()
+        {
+            TimeToBeatText.Content = CurrentTimeText.Content;
+        }
+    }
+}
---
+++ CG_TIMER/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CGT_TIMER
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+#pragma warning disable CS8612 // Nullability of reference types in type doesn't match implicitly implemented member.
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore CS8612 // Nullability of reference types in type doesn't match implicitly implemented member.
+
+        public RelayCommand(Action execute)
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            : this(execute, null)
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        { 
+        }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public RelayCommand(Action execute, Func<bool> canExecute)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+        public bool CanExecute(object parameter)
+#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+#pragma warning disable CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+        public void Execute(object parameter)
+#pragma warning restore CS8767 // Nullability of reference types in type of parameter doesn't match implicitly implemented member (possibly because of nullability attributes).
+        {
+            _execute();
+        }
+    }
+}
